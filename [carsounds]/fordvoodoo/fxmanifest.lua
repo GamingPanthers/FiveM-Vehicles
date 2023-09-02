bdc79a2d2f19b902b4db961c5f6b7704219e0c80
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2023-09-03 07:39:32 +1000

    catch up push

---
+++ [carsounds]/fordvoodoo/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/fordvoodoo_game.dat151.rel',
+  'audioconfig/fordvoodoo_sounds.dat54.rel',
+  'sfx/dlc_fordvoodoo/fordvoodoo.awc',
+  'sfx/dlc_fordvoodoo/fordvoodoo_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/fordvoodoo_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/fordvoodoo_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fordvoodoo'
--- [carsounds]/talam52v10/fxmanifest.lua
+++ [carsounds]/talam52v10/fxmanifest.lua
@@ -1,13 +1,14 @@
-fx_version "adamant"
-game "gta5"
+fx_version 'adamant'
+
+game 'gta5'
 
 files {
-	"audioconfig/*.dat151.rel",
-	"audioconfig/*.dat54.rel",
-	"audioconfig/*.dat10.rel",
-	"sfx/**/*.awc"
+  'audioconfig/talam52v10_game.dat151.rel',
+  'audioconfig/talam52v10_sounds.dat54.rel',
+  'sfx/dlc_talam52v10/talam52v10.awc',
+  'sfx/dlc_talam52v10/talam52v10_npc.awc'
 }
 
-data_file "AUDIO_GAMEDATA" "audioconfig/talam52v10_game.dat"
-data_file "AUDIO_SOUNDDATA" "audioconfig/talam52v10_sounds.dat"
-data_file "AUDIO_WAVEPACK" "sfx/dlc_talam52v10"
+data_file 'AUDIO_GAMEDATA' 'audioconfig/talam52v10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/talam52v10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_talam52v10'
